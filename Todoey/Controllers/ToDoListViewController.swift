c2335888b857270e84f8d0f8f1965aba25042a68
jorgeSaraiv <32471933+jorgeSaraiv@users.noreply.github.com>
2018-11-24 02:21:06 +0000

    Now it's possible to add new categories . Database model changed. Relationships add. Working with queries.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
 
     var itemArray = [Item]()
+    var selectedCategory : Category? {
+        didSet {
+            //gets triggers after value is changed
+            loadItems()
+        }
+    }
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     let defaults = UserDefaults.standard
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadItems()
 
     }
@@ -46,8 +56,16 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //Updating the item
+        //itemArray[indexPath.row].setValue("Completed", forKey: "title")
+        
+        //Toogling item
+        //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
     
+        //Removing
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: indexPath.row)
+        
         saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
@@ -63,11 +81,12 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             //what will happen once user clicks the add item button on our UIAlert
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
+            
             newItem.title = textField.text!
-            
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
-            
             self.saveItems()
             
         }
@@ -88,28 +107,63 @@ class ToDoListViewController: UITableViewController {
     //MARK - Model manipulation Methods
     
     func saveItems(){
-        let encoder = PropertyListEncoder()
+        
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+           try context.save()
         } catch {
-            print("Error encoding item array")
+            print("Error saving context \n \(error)")
         }
         
         tableView.reloadData()
     }
     
-    func loadItems(){
-            if let data =  try? Data(contentsOf: dataFilePath!) {
-                let decoder = PropertyListDecoder()
-                do {
-                    itemArray = try decoder.decode([Item].self, from: data)
-                }catch {
-                    print("Error while loading data, decoding \(error)")
-                }
-            }
-            
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
         
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        request.predicate = categoryPredicate
+            
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        }
+        else {
+            request.predicate = categoryPredicate
+        }
+        
+        do {
+            itemArray = try context.fetch(request)
+        }catch {
+            print("Error fecthing data from text issue: \(error)")
+        }
+        tableView.reloadData()
     }
+    
 }
 
+//MARK: - Search bar methods
+extension ToDoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate:  request.predicate!)
+ 
+
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //when start typing on search bar
+        //when no text this method is triggered too
+        if searchBar.text?.count == 0 { //when we press cross button
+            print("cross was pressed")
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder() //keybaord should go away, go to original state
+            }
+        }
+    }
+}
